6cf32b97814be01220e0a08e5d82f1fa2d1b2aa6
syed zubair ahmed <syedzubairahmed001@gmail.com>
2018-06-05 17:17:07 +0530

    added new animations

--- resourses/js/script.js
+++ resourses/js/script.js
@@ -2,10 +2,15 @@ $(document).ready(function(){
     var userName;
     var userNameH1;
     $(".sec-enter-name-btn").click( function(){
-        $(".enter-name-section").fadeOut("slow");
+        if(document.getElementById("sec-enter-name-input").value === ''){
+            alert("name cannot be empty");
+        }
+        else{
+            $(".enter-name-section").fadeOut("slow");
         userName = document.getElementById("sec-enter-name-input").value;
         userNameH1 = document.getElementById('user-name-h1');
         userNameH1.innerHTML = userName;
+        }
     });
     console.log(userName);
 
