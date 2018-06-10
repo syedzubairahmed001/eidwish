636f4b7af9bcab594b9c576a259d40f79d150bd2
syed zubair ahmed <syedzubairahmed001@gmail.com>
2018-06-10 16:16:43 +0530

    added whatsapp share btn with custom text

--- resourses/js/script.js
+++ resourses/js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-    var userName;
-    var userNameH1;
+    let userName;
+    let userNameH1;
     $(".sec-enter-name-btn").click( function(){
         if(document.getElementById("sec-enter-name-input").value === ''){
             alert("name cannot be empty");
@@ -13,7 +13,41 @@ $(document).ready(function(){
         }
     });
     console.log(userName);
+    // let dataText = "HELLO, *" + userName + "* HAVE SEND A SPECIAL GIFT FOR YOU, *open this, enter your name, and see magic!* and don't forget *" + userName + "* is the first one doing this-->";
+    // $(".whatsapp").attr("data-text") = dataText;
 
+    /* --whatsapp share-*/
+    var isMobile = {
+        Android: function() {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function() {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function() {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function() {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function() {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function() {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        }
+    };
+     $(document).on("click", '.whatsapp', function() {
+            if( isMobile.any() ) {
+                var text = "HELLO, *" + userName + "* HAVE SEND A SPECIAL GIFT FOR YOU, *open this, enter your name, and see magic!* and don't forget *" + userName + "* is the first one doing this-->";
+                var url = $(this).attr("data-link");
+                var message = encodeURIComponent(text) + " - " + encodeURIComponent(url);
+                var whatsapp_url = "whatsapp://send?text=" + message;
+                window.location.href = whatsapp_url;
+            } else {
+                alert("Please share this article in mobile device");
+            }
+        });
 });
 
 // function test(){
